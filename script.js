fdc4c9cba75a303113b4d0a803cd028f23167e86
12345678im <datfat123456@gmail.com>
2025-06-09 19:48:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const { chromium } = require('playwright');
+const fs = require('fs/promises');
+
+(async () => {
+  const phones = (await fs.readFile('phones.txt', 'utf8')).split('\n').filter(Boolean);
+
+  for (const phone of phones) {
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto('https://update.areen.net/');
+
+    await page.fill('input[name="mobile"]', phone);
+
+    await page.click('button[type="submit"]');
+
+    console.log(`Submitted for: ${phone}`);
+
+    await browser.close();
+
+    await new Promise(res => setTimeout(res, 5 * 60 * 1000));
+  }
+})();
